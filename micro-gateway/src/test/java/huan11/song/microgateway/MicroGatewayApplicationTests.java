851a4120cfe780335bbb41f896b093f31a214e67
songhuan1120 <15822021353@163.com>
2020-04-27 11:57:24 +0800

    网关工程的pom文件

---
+++ micro-gateway/src/test/java/huan11/song/microgateway/MicroGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package huan11.song.microgateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
